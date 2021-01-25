62892364150082f27bf879b2aa693112e3a08d8b
Rik <hoogendoorn.rik@gmail.com>
2021-01-25 15:53:53 +0100

    Glideslope af op zie description na
    amma/theta klopt nog niet

--- transfer_functions_ILS.m
+++ transfer_functions_ILS.m
@@ -1,12 +1,12 @@
 import = load('SS_ILS.mat')
 SS_ILS = import.SS_long
 
-SS_ILS.C = [0 0 1 0 0]
+SS_ILS.C = [1 0 0 0 0]
 [num_alpha,den_alpha] = ss2tf(SS_ILS.A, SS_ILS.B, SS_ILS.C, SS_ILS.D, 1)
 
 SS_ILS.C = [0 0 0 1 0]
 [num_theta,den_theta] = ss2tf(SS_ILS.A, SS_ILS.B, SS_ILS.C, SS_ILS.D, 1)
 
-transfer_function = 1 - tf(num_alpha, num_theta)
+transfer_function = tf(num_alpha, den_alpha)
 
-save transfer_functions_ILS.mat transfer_function ;
+save transfer_functions_ILSss.mat transfer_function ;
