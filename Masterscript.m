da9b0077f44898d14d0cb4e3f17b2a23a11b1da4
n-v-amstel <61739607+n-v-amstel@users.noreply.github.com>
2021-02-05 15:30:10 +0100

    alpha and theta loops separated

--- Masterscript.m
+++ Masterscript.m
@@ -5,14 +5,15 @@ close all
 %% Define Constants
 w_1 = 0.1 %gain
 Gamma_ref = 0 % should always be zero
+alpha_ref = 0 
 gamma = 3 %[deg]
-Initial_elevator = -4.1 %[deg]
+Initial_elevator = -4.1891 %[deg]
 H_0 = 5000 %[ft]
 H   = 5000  %[ft] actual altitude
 V_ref = 300 %[ft/s]
 R_init = 6737.5 %[ft] 5000ft altitude, 8 miles away from RWY 
-Initial_throttle = 1000 %[lb] initial thrust setting
-TF = 5 % dummy simulation time per 'iteration'
+Initial_throttle = 2826.8165 %[lb] initial thrust setting
+TF = 10 % dummy simulation time per 'iteration'
 H_flare = 3050 %[ft] = 50 ft AGL
 t_sim = 0 %[s] simulation time
 tau = 1.1233 %[s] time constant for flare mode
