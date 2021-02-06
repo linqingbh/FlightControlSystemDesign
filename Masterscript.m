fb959dd01eef252239b804455389fec03b05ecd9
n-v-amstel <61739607+n-v-amstel@users.noreply.github.com>
2021-02-06 20:47:44 +0100

    stable_until_K_theta

--- Masterscript.m
+++ Masterscript.m
@@ -6,14 +6,14 @@ close all
 w_1 = 0.1 %gain
 Gamma_ref = 0 % should always be zero
 alpha_ref = 0 
-gamma = 3 %[deg]
+gamma_r = 3/180*pi %[deg]
 Initial_elevator = -4.1891 %[deg]
 H_0 = 5000 %[ft]
 H   = 5000  %[ft] actual altitude
 V_ref = 300 %[ft/s]
-R_init = 6737.5 %[ft] 5000ft altitude, 8 miles away from RWY 
+R_init = 38214 %[ft] 5000ft altitude, 8 miles away from RWY 
 Initial_throttle = 2826.8165 %[lb] initial thrust setting
-TF = 10 % dummy simulation time per 'iteration'
+TF = 1000 % dummy simulation time per 'iteration'
 H_flare = 3050 %[ft] = 50 ft AGL
 t_sim = 0 %[s] simulation time
 tau = 1.1233 %[s] time constant for flare mode
